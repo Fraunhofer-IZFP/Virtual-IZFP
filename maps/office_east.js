b4ad1a153a02e5ce290cf176350a8cc66e8a9321
fabiankrieg <32844842+fabiankrieg@users.noreply.github.com>
2021-12-08 09:50:41 +0000

    Deploying to gh-pages from master @ 385e50200a35ead2933d76519e7fbdd0fa6caa05 🚀

--- maps/office_east.js
+++ maps/office_east.js
@@ -1,5 +1,5 @@
 let helloWorldPopup;
-
+/*
 // Open the popup when we enter a given zone
 helloWorldPopup = WA.room.onEnterZone('silent_zone_pop_bottom', () => {
     WA.ui.openPopup("myPopup1", 'Hello world!', [])});
@@ -7,7 +7,27 @@ helloWorldPopup = WA.room.onEnterZone('silent_zone_pop_bottom', () => {
 // Close the popup when we leave the zone.
 WA.room.onLeaveZone('silent_zone_pop_bottom', () => {
     helloWorldPopup.close();
+});*/
+
+
+let helloWorldPopup;
+
+// Open the popup when we enter a given zone
+helloWorldPopup = WA.room.onEnterLayer("silent_zone_pop_bottom").subscribe(() => {
+    WA.ui.openPopup("myPopup1", 'Hello world!', [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }]);
 });
+
+// Close the popup when we leave the zone.
+WA.room.onLeaveLayer("silent_zone_pop_bottom").subscribe(() => {
+    helloWorldPopup.close();
+})
 
 
 
