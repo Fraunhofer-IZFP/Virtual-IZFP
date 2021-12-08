302d47e2cd796bf63114a59ba81f92c5e6802bcb
Ganster, Michael <michael.ganster@izfp.fraunhofer.de>
2021-12-08 11:22:08 +0100

    another fix

--- maps/beach.js
+++ maps/beach.js
@@ -2,7 +2,7 @@
     // WA.chat.sendChatMessage("Hier herrscht Stille. Absolute Stille!", 'Baderegeln');
 // })
 
-helloWorldPopup = WA.room.onEnterZone("popup_sign_entry").subscribe(() => {
+WA.room.onEnterZone('popup_sign_entry', () => {
     WA.ui.openPopup("SignPopup", 'Hello world!', [{
         label: "Close",
         className: "primary",
@@ -11,8 +11,19 @@ helloWorldPopup = WA.room.onEnterZone("popup_sign_entry").subscribe(() => {
             popup.close();
         }
     }]);
-});
+})
 
-WA.room.onLeaveZone("popup_sign_entry").subscribe(() => {
-    helloWorldPopup.close();
-});
+// helloWorldPopup = WA.room.onEnterZone("popup_sign_entry").subscribe(() => {
+    // WA.ui.openPopup("SignPopup", 'Hello world!', [{
+        // label: "Close",
+        // className: "primary",
+        // callback: (popup) => {
+            // // Close the popup when the "Close" button is pressed.
+            // popup.close();
+        // }
+    // }]);
+// });
+
+// WA.room.onLeaveZone("popup_sign_entry").subscribe(() => {
+    // helloWorldPopup.close();
+// });
