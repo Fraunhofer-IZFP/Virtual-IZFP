40e044db8d0da27ef10df505888b280375ccf29e
fuchsteufel <mganster@freenet.de>
2021-12-08 10:18:56 +0000

    Deploying to gh-pages from master @ 388f200c3ac29bc34fd20c1f749888e5feff0f80 🚀

--- maps/beach.js
+++ maps/beach.js
@@ -1,3 +1,18 @@
-WA.room.onEnterZone('popup_sign_entry', () => {
-    WA.chat.sendChatMessage("Hier herrscht Stille. Absolute Stille!", 'Baderegeln');
-})
+// WA.room.onEnterZone('popup_sign_entry', () => {
+    // WA.chat.sendChatMessage("Hier herrscht Stille. Absolute Stille!", 'Baderegeln');
+// })
+
+helloWorldPopup = WA.room.onEnterZone("popup_sign_entry").subscribe(() => {
+    WA.ui.openPopup("SignPopup", 'Hello world!', [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }]);
+});
+
+WA.room.onLeaveZone("popup_sign_entry").subscribe(() => {
+    helloWorldPopup.close();
+});
