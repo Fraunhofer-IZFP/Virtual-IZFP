d45143fc3ea21e4146d4607987264e4b668c0731
fuchsteufel <mganster@freenet.de>
2021-12-08 10:36:33 +0000

    Deploying to gh-pages from master @ 411c74f917ace99636ec12e8d34852adfc332504 🚀

--- maps/beach.js
+++ maps/beach.js
@@ -2,19 +2,8 @@
     // WA.chat.sendChatMessage("Hier herrscht Stille. Absolute Stille!", 'Baderegeln');
 // })
 
-WA.room.onEnterZone('popup_sign_entry', () => {
-	WA.chat.sendChatMessage("Hier herrscht Stille. Absolute Stille!", 'Baderegeln');
-    WA.ui.openPopup("SignPopup", 'Hello world!', [{
-        label: "Close",
-        className: "primary",
-        callback: (popup) => {
-            // Close the popup when the "Close" button is pressed.
-            popup.close();
-        }
-    }]);
-})
-
-// helloWorldPopup = WA.room.onEnterZone("popup_sign_entry").subscribe(() => {
+// WA.room.onEnterZone('popup_sign_entry', () => {
+	// WA.chat.sendChatMessage("Hier herrscht Stille. Absolute Stille!", 'Baderegeln');
     // WA.ui.openPopup("SignPopup", 'Hello world!', [{
         // label: "Close",
         // className: "primary",
@@ -23,7 +12,19 @@ WA.room.onEnterZone('popup_sign_entry', () => {
             // popup.close();
         // }
     // }]);
-// });
+// })
+
+helloWorldPopup = WA.room.onEnterZone('popup_sign_entry').subscribe(() => {
+	WA.chat.sendChatMessage("Hier herrscht Stille. Absolute Stille!", 'Baderegeln');
+    WA.ui.openPopup('SignPopup', 'Hello world!', [{
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }]);
+});
 
 // WA.room.onLeaveZone("popup_sign_entry").subscribe(() => {
     // helloWorldPopup.close();
