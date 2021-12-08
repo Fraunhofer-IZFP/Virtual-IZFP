a9abcb02c5bef1cc72200980bc75bdb6403a24e6
fuchsteufel <mganster@freenet.de>
2021-12-08 11:10:52 +0000

    Deploying to gh-pages from master @ 9a57b44d14cbcbea630205533449728d1fc899d3 🚀

--- maps/beach.js
+++ maps/beach.js
@@ -1,36 +1,25 @@
-WA.room.onEnterZone('popup_sign_entry', () => {
-    WA.chat.sendChatMessage("Hier herrscht Stille. Absolute Stille!", 'Baderegeln');
-	WA.ui.openPopup("SignPopup", 'Test eins zwo', []);
+var targetObjectSign ='PopupSign';
+var popUpSign = undefined;
+
+
+WA.onEnterZone('popup_sign_entry', () => {
+	WA.chat.sendChatMessage("Hier herrscht Stille. Absolute Stille!", 'Baderegeln');
+	
+	popUpSign = WA.openPopup(targetObjectSign, 'Hello World', [
+		{
+			label: "Close",
+			className: "popUpElement",
+			callback: (popup) => {
+				popup.close();
+			}
+		}
+	])
+});
+
+WA.onLeaveZone('popup_sign_entry', () => {
+    if (popUpSign !== undefined) popUpSign.close();
 })
 
 // WA.room.onEnterZone('popup_sign_entry', () => {
     // WA.chat.sendChatMessage("Hier herrscht Stille. Absolute Stille!", 'Baderegeln');
 // })
-
-// WA.room.onEnterZone('popup_sign_entry', () => {
-	// WA.chat.sendChatMessage("Hier herrscht Stille. Absolute Stille!", 'Baderegeln');
-    // WA.ui.openPopup("SignPopup", 'Hello world!', [{
-        // label: "Close",
-        // className: "primary",
-        // callback: (popup) => {
-            // // Close the popup when the "Close" button is pressed.
-            // popup.close();
-        // }
-    // }]);
-// })
-
-// helloWorldPopup = WA.room.onEnterZone('popup_sign_entry').subscribe(() => {
-	// WA.chat.sendChatMessage("Hier herrscht Stille. Absolute Stille!", 'Baderegeln');
-    // WA.ui.openPopup('SignPopup', 'Hello world!', [{
-        // label: "Close",
-        // className: "primary",
-        // callback: (popup) => {
-            // // Close the popup when the "Close" button is pressed.
-            // popup.close();
-        // }
-    // }]);
-// });
-
-// WA.room.onLeaveZone("popup_sign_entry").subscribe(() => {
-    // helloWorldPopup.close();
-// });
