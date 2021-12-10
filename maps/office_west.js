bf1f303cebd0089a94c60c3c72c97893597245c1
fuchsteufel <mganster@freenet.de>
2021-12-10 12:36:41 +0000

    Deploying to gh-pages from master @ be2450d717296ba4a2a1fd663f6fdde2c805c4ad 🚀

--- maps/office_west.js
+++ maps/office_west.js
@@ -1,4 +1,14 @@
 // important variables
+triggerZoneChuckNorris = 'popup_chuck_norris'
+targetRectangleChuckNorris = 'PopupChuckNorris'
+
+chuck_norris_jokes_german = [
+	'Chuck Norris isst keinen Honig, er kaut Bienen!',
+	'Wie viele Liegestütze schafft Chuck Norris? Alle!'
+	]
+chuck_norris_jokes_english = [
+	'Chuck Norris\' tears can cure cancer. Too bad he never cries!'
+	]
 
 msg_dining_room = 'Jitsi dining room'
 msg_market_farm = 'North: farm\nEast: market'
@@ -7,6 +17,7 @@ msg_quiet_area = 'Silent area'
 msg_lib = 'Library'
 msg_throne = 'Audience room'
 
+var isEnglish = false
 
 // helper scripts
 
@@ -31,6 +42,46 @@ function generate_popup(target_zone, target_rectangle, popup_text){
 		if (popUpExplanation !== undefined) popUpExplanation.close();
 	})
 }
+
+function showPopupEnglishGerman(target_zone, target_rectangle, popup_content_english, popup_content_german){
+	var popUpInstance = undefined;
+	WA.onEnterZone(target_zone, () => {
+		var index = Math.floor(Math.random() * Math.min(popup_content_english.length, popup_content_german.length));
+		// get language
+		if(isEnglish)
+		{
+			popUpInstance = WA.openPopup(target_rectangle, popup_content_english[index], 
+			[{
+				label: "Deutsch",
+				className: "primary",
+				callback: (popup) => {
+					// Close the popup when the "Close" button is pressed.
+					popup.close();
+					isEnglish = false;
+				}
+			}]);
+		}
+		else 
+		{
+			popUpInstance = WA.openPopup(target_rectangle, popup_content_german[index], 
+			[{
+				label: "English",
+				className: "primary",
+				callback: (popup) => {
+					// Close the popup when the "Close" button is pressed.
+					popup.close();
+					isEnglish = true;
+				}
+			}]);
+		}
+	});
+	WA.onLeaveZone(target_zone, () => {
+		if (popUpInstance !== undefined) popUpInstance.close();
+	})
+}
+
+// multi language popups
+showPopupEnglishGerman(triggerZoneChuckNorris, targetRectangleChuckNorris, chuck_norris_jokes_english, chuck_norris_jokes_german)
 
 
 // pop-up-generation
