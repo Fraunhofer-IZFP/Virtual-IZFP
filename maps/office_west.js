ed7bfd0aa55275d75edb2ce8276084508fde1ae8
Ganster, Michael <michael.ganster@izfp.fraunhofer.de>
2021-12-10 13:46:17 +0100

    translated office west signs

--- maps/office_west.js
+++ maps/office_west.js
@@ -10,12 +10,28 @@ chuck_norris_jokes_english = [
 	'Chuck Norris\' tears can cure cancer. Too bad he never cries!'
 	]
 
-msg_dining_room = 'Jitsi dining room'
-msg_market_farm = 'North: farm\nEast: market'
-msg_map_east = 'To the eastern offices'
-msg_quiet_area = 'Silent area'
-msg_lib = 'Library'
-msg_throne = 'Audience room'
+msg_dining_room_english = 'Jitsi dining room'
+msg_market_farm_english = 'North: farm\nEast: market'
+msg_map_east_english = 'To the eastern offices'
+msg_quiet_area_english = 'Silent area'
+msg_lib_english = 'Library'
+msg_throne_english = 'Audience room'
+
+
+msg_dining_room_german = 'Jitsi-Esszimmer'
+msg_market_farm_german = 'Norden: Bauernhof\nOsten: Markt'
+msg_map_east_german = 'Zu den östlichen Büros'
+msg_quiet_area_german = 'Stille Ecke'
+msg_lib_german = 'Bibliothek'
+msg_throne_german = 'Thronsaal'
+
+showPopupEnglishGerman('zone_dining_room', 'rect_dining_room', msg_dining_room_english, msg_dining_room_german)
+showPopupEnglishGerman('zone_market_and_farm', 'rect_market_and_farm', msg_market_farm_english, msg_market_farm_german)
+showPopupEnglishGerman('zone_map_east_2', 'rect_map_east_2', msg_map_east_english, msg_map_east_german)
+showPopupEnglishGerman('zone_map_east', 'rect_map_east', msg_map_east_english, msg_map_east_german)
+showPopupEnglishGerman('zone_quiet_area', 'rect_quiet_area', msg_quiet_area_english, msg_quiet_area_german)
+showPopupEnglishGerman('zone_lib', 'rect_lib', msg_lib_english, msg_lib_german)
+showPopupEnglishGerman('zone_throne', 'rect_throne', msg_throne_english, msg_throne_german)
 
 var isEnglish = false
 
@@ -85,15 +101,15 @@ showPopupEnglishGerman(triggerZoneChuckNorris, targetRectangleChuckNorris, chuck
 
 
 // pop-up-generation
-generate_popup('zone_dining_room', 'rect_dining_room', msg_dining_room)
+// generate_popup('zone_dining_room', 'rect_dining_room', msg_dining_room)
 
-generate_popup('zone_market_and_farm', 'rect_market_and_farm', msg_market_farm)
+// generate_popup('zone_market_and_farm', 'rect_market_and_farm', msg_market_farm)
 
-generate_popup('zone_map_east_2', 'rect_map_east_2', msg_map_east)
-generate_popup('zone_map_east', 'rect_map_east', msg_map_east)
+// generate_popup('zone_map_east_2', 'rect_map_east_2', msg_map_east)
+// generate_popup('zone_map_east', 'rect_map_east', msg_map_east)
 
-generate_popup('zone_quiet_area', 'rect_quiet_area', msg_quiet_area)
+// generate_popup('zone_quiet_area', 'rect_quiet_area', msg_quiet_area)
 
-generate_popup('zone_lib', 'rect_lib', msg_lib)
+// generate_popup('zone_lib', 'rect_lib', msg_lib)
 
-generate_popup('zone_throne', 'rect_throne', msg_throne)
+// generate_popup('zone_throne', 'rect_throne', msg_throne)
